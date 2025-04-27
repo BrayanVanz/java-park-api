97cce4aabe4744b4e9885cda6bcc118c58437806
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-04-27 02:07:52 -0300

    Test save user exceptions

--- src/test/java/com/brayanvanz/park_api/UserIT.java
+++ src/test/java/com/brayanvanz/park_api/UserIT.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import com.brayanvanz.park_api.dtos.UserResponseDto;
 import com.brayanvanz.park_api.dtos.UserSaveDto;
 import com.brayanvanz.park_api.enums.Role;
+import com.brayanvanz.park_api.exceptions.ErrorMessage;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -36,5 +37,89 @@ public class UserIT {
         Assertions.assertThat(responseBody.getId()).isNotNull();
         Assertions.assertThat(responseBody.getUsername()).isEqualTo("yugi@gmail.com");
         Assertions.assertThat(responseBody.getRole()).isEqualTo(Role.CLIENT);
+    }
+
+    @Test
+    public void save_InvalidUsername_ReturnErrorMessageStatus422() {
+        ErrorMessage responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("", "123456"))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("yugi@", "123456"))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("yugi@gmail.", "123456"))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void save_InvalidPassword_ReturnErrorMessageStatus422() {
+        ErrorMessage responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("yugi@gmail.com", ""))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("yugi@gmail.com", "123"))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("yugi@gmail.com", "123456789"))
+            .exchange()
+            .expectStatus().isEqualTo(422)
+            .expectBody(ErrorMessage.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 }
