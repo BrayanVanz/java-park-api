58327d92911e7d18dda4cd4e8b8b4dfeee2804d9
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-04-27 01:47:08 -0300

    Test save new user

--- src/test/java/com/brayanvanz/park_api/UserIT.java
+++ src/test/java/com/brayanvanz/park_api/UserIT.java
@@ -1,9 +1,16 @@
 package com.brayanvanz.park_api;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.brayanvanz.park_api.dtos.UserResponseDto;
+import com.brayanvanz.park_api.dtos.UserSaveDto;
+import com.brayanvanz.park_api.enums.Role;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -12,4 +19,22 @@ public class UserIT {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    public void save_ValidUsernameAndPassword_ReturnCreatedUserStatus201() {
+        UserResponseDto responseBody = webTestClient
+            .post()
+            .uri("/api/v1/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new UserSaveDto("yugi@gmail.com", "123456"))
+            .exchange()
+            .expectStatus().isCreated()
+            .expectBody(UserResponseDto.class)
+            .returnResult().getResponseBody();
+        
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getId()).isNotNull();
+        Assertions.assertThat(responseBody.getUsername()).isEqualTo("yugi@gmail.com");
+        Assertions.assertThat(responseBody.getRole()).isEqualTo(Role.CLIENT);
+    }
 }
--- src/test/resources/sql/users/users-insert.sql
+++ src/test/resources/sql/users/users-insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (id, username, password, role) VALUES (100, "joey@gmail.com", "123456", "ADMIN");
-INSERT INTO users (id, username, password, role) VALUES (101, "tea@gmail.com", "123456", "CLIENT");
-INSERT INTO users (id, username, password, role) VALUES (102, "tristan@gmail.com", "123456", "CLIENT");
+INSERT INTO users (id, username, password, role) VALUES (100, 'joey@gmail.com', '123456', 'ADMIN');
+INSERT INTO users (id, username, password, role) VALUES (101, 'tea@gmail.com', '123456', 'CLIENT');
+INSERT INTO users (id, username, password, role) VALUES (102, 'tristan@gmail.com', '123456', 'CLIENT');
