1dc9dd2935c378e43b6b13214f4ecf7eb2dfb73e
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-05-04 21:36:05 -0300

    Create ClientParkingSpace

---
+++ src/main/java/com/brayanvanz/park_api/entities/ClientParkingSpace.java
@@ -0,0 +1,114 @@
+package com.brayanvanz.park_api.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "client_parking_spaces")
+@EntityListeners(AuditingEntityListener.class)
+public class ClientParkingSpace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 15)
+    private String receipt;
+
+    @Column(nullable = false, length = 8)
+    private String plate;
+
+    @Column(nullable = false, length = 45)
+    private String brand;
+
+    @Column(nullable = false, length = 45)
+    private String model;
+
+    @Column(nullable = false, length = 45)
+    private String color;
+
+    @Column(name = "entry_date", nullable = false)
+    private LocalDateTime entryDate;
+
+    @Column(name = "exit_date", nullable = true)
+    private LocalDateTime exitDate;
+
+    @Column(columnDefinition = "decimal(7,2)")
+    private BigDecimal value;
+
+    @Column(columnDefinition = "decimal(7,2)")
+    private BigDecimal discount;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client", nullable = false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "id_parking_space", nullable = false)
+    private ParkingSpace parkingSpace;
+
+    @CreatedDate
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @LastModifiedDate
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ClientParkingSpace other = (ClientParkingSpace) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
